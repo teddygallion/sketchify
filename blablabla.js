5e3a9f5b38922b3fd34397d6da39924da5a23e50
Teddy Gallion <ted.j.gallion@gmail.com>
2023-12-17 13:37:10 -0800

    fixed mouse tracking, worked on css styling

--- blablabla.js
+++ blablabla.js
@@ -1,7 +1,6 @@
 const colorPicker = document.getElementById("color");
 const clearBtn = document.getElementById('clear-btn');
-const lineWidthBtn = document.getElementById('line-width');
-const mousePos = document.getElementById('mouse-position');
+
 
 
 class DrawingApp {
@@ -12,55 +11,51 @@ class DrawingApp {
 		this.lastX = 0;
 		this.lastY = 0;
 		this.currentColor = '#000';
-		this.lineWidth = 5;
+
 		this.setup();
 	}
 	startDrawing(e) {
 		this.isDrawing = true;
-		 this.isDrawing = true;
-	    this.context.beginPath();
-	    this.context.moveTo(e.offsetX, e.offsetY);
+		[this.lastX, this.lastY] = [e.offsetX, e.offsetY];
 	}
 	draw(e){
 		if(!this.isDrawing) return;
+		this.context.strokeStyle = this.currentColor;
+		this.context.lineCap = 'round';
+		this.context.lineJoin = 'round';
+		this.context.lineWidth = 2;
+
+		this.context.beginPath();
+		this.context.moveTo(this.lastX, this.lastY);
+		console.log(`${this.lastX}, ${this.lastY}`)
 		this.context.lineTo(e.offsetX, e.offsetY);
-		this.lineWidth = this.lineWidthValue;
-	    this.context.strokeStyle = this.currentColor;
-	    this.context.lineCap = 'round';
-	    this.context.lineJoin = 'round';
-	    this.context.stroke();	
+
+		this.context.stroke();
+
+		[this.lastX, this.lastY] = [e.offsetX, e.offsetY];
 	}
 	stopDrawing(){
 		this.isDrawing = false;
-		this.context.closePath();
 	}
-
 	clearCanvas(e){
 		e.preventDefault();
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	}
-
 	changeColor(newColor){
-
 		this.currentColor = newColor;
 	}
-	changeLineWidth(newLineWidth){
-		this.lineWidth = newLineWidth;
-	}
+
 
 	setup(){
-		this.canvas.addEventListener('mousemove', (e) => this.watchMouse(e));
 		clearBtn.addEventListener('click', (e)=> this.clearCanvas(e));
 		colorPicker.addEventListener('change', (e)=> this.changeColor(colorPicker.value));
-		//lineWidthBtn.addEventListener('change',(e) => this.changeLineWidth(lineWidthBtn.value));
 		this.canvas.addEventListener('mousedown', (e) => this.startDrawing(e));
 		this.canvas.addEventListener('mousemove', (e) => this.draw(e));
 		this.canvas.addEventListener('mouseup', () => this.stopDrawing());
 		this.canvas.addEventListener('mouseout', () => this.stopDrawing());
 	}
 }
-
-const drawingApp = new DrawingApp('canvas');
+const drawingApp = new DrawingApp('myCanvas');
 
 function changeColor(color){
 	drawingApp.changeColor(color);
@@ -69,3 +64,7 @@ function changeColor(color){
 function clearCanvas(){
 	drawingApp.clearCanvas();
 }
+
+
+
+
---
+++ devlog.md
@@ -0,0 +1,64 @@
+# Module 2 Final Project Dev log
+
+## 12/04/2023:
+**7:30pm - 9:30pm** 
+
+	- Brainstormed ideas for app concept
+	- Background research for HTML canvas tag, utilities.[^1]
+	- Researched options for picking colors in HTML
+
+## 12/05/2023:
+**10:00am - 12:00pm** 
+
+	- Built wireframe of site
+	- Implemented drawing functions and clear canvas functionality.
+	- Added color change function, implemented UI for picking color. 
+
+**1:00pm - 4:00pm** 
+
+	- Research on how mouse movements are registered in-browser to improve mouse tracking. 
+	- Background research on other implementations of js drawing apps to 
+		determine how to best implement mouse tracking[^1] (see footnotes below for sources) [!4]
+
+## 12/08/2023
+**10:00am - 12:00pm** 
+
+	- Worked on css styling for canvas/toolbar positioning. 
+
+**12:00pm - 4:00pm** 
+
+	- More work on UI/styling using flexbox
+	- Additional background research on HTML canvas elements and user events. [^2][^3]
+
+## 12/10/2023 
+**10:00am - 3:00pm** 
+	
+	- Further research on JS mouse events [^4]
+	- For purposes of fixing my drawing tool I tried about six different mouse position properties,
+		printing their values to the screen and using these values to set cursor position.  
+	- These methods either did not improve cursor accuracy or made accuracy worse. 
+
+# 12/11/2023
+**4:30pm - 6:30pm** 
+
+	- Further research on other implementations of JS drawing apps [^5][^6]
+	without using external libraries or frameworks. 
+	- At this point I was lost as to what could be going wrong, so I shifted focus 
+	  to other aspects of my project, i.e. CSS styling, accessibility features, 
+	  and brainstorming stretch goals. 
+
+# 12/12/2023
+**4:30 - 6:30pm** 
+
+	- Found out exactly what was going wrong with my canvas tag, implemented appropriate solution, refactoring code. 
+
+## Works Cited: 
+
+[^1]: MDN: Mouse Events (https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/)
+[^2]: MDN: Canvas API (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+[^3]: W3Schools: Canvas HTML Element (https://www.w3schools.com/html/html5_canvas.asp)
+[^4]: 'Create a drawing app using JavaScript and canvas', Adam Nagy, 2021 (https://dev.to/javascriptacademy/create-a-drawing-app-using-javascript-and-canvas-2an1)
+[^5]: 'Drawing Canvas Paint in Vanilla JS' by Codepen user' Yananas, date unknown(https://codepen.io/yananas/pen/rwvZvY)
+
+
+
